d7210660bfb9de2752bef8722b67fb6eca0c68b3
João Gabriel Z.B <71906858+JoaoGabrielBertolucci@users.noreply.github.com>
2023-06-13 18:22:13 -0300

    criação e edição de arquivos

---
+++ backendefornecedores/src/main/java/fornecedores/com/backendefornecedores/BackendefornecedoresApplication.java
@@ -0,0 +1,13 @@
+package fornecedores.com.backendefornecedores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendefornecedoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendefornecedoresApplication.class, args);
+	}
+
+}
---
+++ backendefornecedores/src/test/java/fornecedores/com/backendefornecedores/BackendefornecedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package fornecedores.com.backendefornecedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendefornecedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
